6a8264315a4562f394ba6d3120d32c53bd18eda8
peshk1n <anna_konoplyanova@mail.ru>
2025-03-17 03:11:08 +0300

    Add progress bar

--- app/src/main/java/com/example/filearchiver/FileListActivity.java
+++ app/src/main/java/com/example/filearchiver/FileListActivity.java
@@ -3,9 +3,12 @@ package com.example.filearchiver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
@@ -13,38 +16,49 @@ import java.util.ArrayList;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 public class FileListActivity extends AppCompatActivity {
 
     static {
-        System.loadLibrary("filearchiver"); // Загружаем нативную библиотеку
+        System.loadLibrary("filearchiver");
     }
     private ArrayList<Uri> fileUris;
     private FileAdapter fileAdapter;
+    private ProgressBar progressBar;
+    private TextView progressText;
+    private Button btnArchive;
 
     private int totalFiles; // Общее количество файлов
     private int filesArchived = 0; // Количество заархивированных файлов
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_list);
 
-        // Получаем общее количество файлов
         fileUris = getIntent().getParcelableArrayListExtra("selectedFiles");
         totalFiles = fileUris != null ? fileUris.size() : 0;
 
-        RecyclerView fileRecyclerView = findViewById(R.id.fileRecyclerView);
-        Button btnArchive = findViewById(R.id.archiveButton);
-
-        fileUris = getIntent().getParcelableArrayListExtra("selectedFiles");
-
         // Настройка RecyclerView
+        RecyclerView fileRecyclerView = findViewById(R.id.fileRecyclerView);
         fileRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         fileAdapter = new FileAdapter(getFileItems());
         fileRecyclerView.setAdapter(fileAdapter);
 
+        progressBar = findViewById(R.id.progressBar);
+        progressText = findViewById(R.id.progressText);
+        btnArchive = findViewById(R.id.archiveButton);
+
         // Обработчик кнопки архивировации
         btnArchive.setOnClickListener(v -> {
+            btnArchive.setVisibility(View.GONE);
+            progressBar.setVisibility(View.VISIBLE);
+            progressBar.setProgress(0);
+            progressText.setVisibility(View.VISIBLE);
+            progressText.setText("File " +  "0/" + totalFiles);
+            filesArchived = 0;
+
             // Логика архивации
             if (fileUris != null && !fileUris.isEmpty()) {
                 for (Uri fileUri : fileUris) {
@@ -54,7 +68,6 @@ public class FileListActivity extends AppCompatActivity {
                 // Обработка случая, когда список пустой
                 Toast.makeText(this, "Файлы не выбраны", Toast.LENGTH_SHORT).show();
             }
-            filesArchived = 0;
         });
     }
 
@@ -63,8 +76,15 @@ public class FileListActivity extends AppCompatActivity {
     public void updateFileProgress() {
         filesArchived++;
         int progress = (int) ((filesArchived * 100.0) / totalFiles); // Прогресс в процентах
-        Log.d("FileArchiver", "Обработано файлов: " + filesArchived + " из " + totalFiles);
+        progressBar.setProgress(progress);
+        progressText.setText("File " +  filesArchived + "/" + totalFiles);
+        if (filesArchived == totalFiles) {
+            progressBar.setVisibility(View.GONE);
+            progressText.setVisibility(View.GONE);
+            btnArchive.setVisibility(View.VISIBLE);
+        }
     }
+
 
     // Метод для создания списка FileItem
     private ArrayList<FileItem> getFileItems() {
@@ -85,6 +105,7 @@ public class FileListActivity extends AppCompatActivity {
             String inputFilePath,
             String outputFilePath
     );
+
 
     // Метод для вызова архивации
     private void compressSelectedFile(Uri fileUri) {
@@ -113,10 +134,9 @@ public class FileListActivity extends AppCompatActivity {
         boolean success = compressFile(inputFilePath, outputFilePath);
 
         if (success) {
-            Toast.makeText(this, "Файл успешно заархивирован: " + outputFilePath, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Файл успешно заархивирован: " + outputFilePath, Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, "Ошибка при архивации файла", Toast.LENGTH_SHORT).show();
         }
     }
 }
-
